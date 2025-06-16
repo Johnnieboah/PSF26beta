54a2f3905952dbe4dcf703c4fd97ca8c64088aee
Johnnieboah <johnnieestep21@gmail.com>
2025-06-16 13:36:07 -0500

    Initial Commit

---
+++ PSF26/PSF26App.swift
@@ -0,0 +1,17 @@
+//
+//  PSF26App.swift
+//  PSF26
+//
+//  Created by Jay Estep on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct PSF26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PSF26Tests/PSF26Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PSF26Tests.swift
+//  PSF26Tests
+//
+//  Created by Jay Estep on 6/16/25.
+//
+
+import Testing
+@testable import PSF26
+
+struct PSF26Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
